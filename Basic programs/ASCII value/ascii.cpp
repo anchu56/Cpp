aa9bc83287de4e4786e2a19bb48ebc4fa3eeefba
Ansaf Muhammed <anchu56@gmail.com>
2022-08-18 10:12:01 +0530

    ASCII value of a character

---
+++ Basic programs/ASCII value/ascii.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char a;
+    cout<<"Enter a character \n";
+    cin>>a;
+    cout<<"The ASCII value of the inputed character is "<<int(a);
+    return 0;
+}
