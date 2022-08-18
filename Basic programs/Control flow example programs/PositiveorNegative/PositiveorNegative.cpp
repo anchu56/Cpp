55672a53b4cd54fa4d3db80b1c23543782dcc42f
Ansaf Muhammed <anchu56@gmail.com>
2022-08-18 12:49:31 +0530

    Positive or Negative

---
+++ Basic programs/Control flow example programs/PositiveorNegative/PositiveorNegative.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a number \n";
+    cin>>n;
+    if(n>0){
+        cout<<"The given number "<<n<<" is a positive number";
+    }
+    else{
+        cout<<"The given number "<<n<<" is a negative number";
+    }
+    return 0;
+}
