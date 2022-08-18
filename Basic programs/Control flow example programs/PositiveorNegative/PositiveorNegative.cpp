fa4573a0fd091e60baa39817987c167fee4f2b1d
Ansaf Muhammed <anchu56@gmail.com>
2022-08-18 12:58:07 +0530

    Sum of n natural numbers

--- Basic programs/Control flow example programs/PositiveorNegative/PositiveorNegative.cpp
+++ Basic programs/Control flow example programs/PositiveorNegative/PositiveorNegative.cpp
@@ -5,7 +5,11 @@ int main()
     int n;
     cout<<"Enter a number \n";
     cin>>n;
-    if(n>0){
+    if(n>=0){
+        if(n==0){
+             cout<<"The given number is zero";
+        }
+        else
         cout<<"The given number "<<n<<" is a positive number";
     }
     else{
