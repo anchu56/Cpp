ec6be43b094922137440c0a271fdd3551db6ba5e
Ansaf Muhammed <anchu56@gmail.com>
2022-08-19 06:48:21 +0530

    Prime Or Not

---
+++ Basic programs/Control flow example programs/PrimeorNot/Prime.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,flag=0;
+    cout<<"Enter a number \n";
+    cin>>n;
+    
+    for(int i=2;i<=n/2;i++){
+        if(n%i==0){
+            flag=1;
+            break;
+        }
+    }       
+    if(flag==1){
+        cout<<"It is not a prime number";
+    }
+    else if(n==1){
+        cout<<"It is neither a prime nor a composite number";
+        }
+    else{
+        cout<<"It is a prime number ";
+    }
+    return 0;
+}
